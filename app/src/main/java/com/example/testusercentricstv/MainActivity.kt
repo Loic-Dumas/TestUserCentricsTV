0146343e8c64faa8b783fe155b02bc5bf8a7141e
Loïc Dumas <loic.dumas02@gmail.com>
2023-06-22 14:12:15 +0200

    Make main activity simpler by removing all Android studio boiler plate

--- app/src/main/java/com/example/testusercentricstv/MainActivity.kt
+++ app/src/main/java/com/example/testusercentricstv/MainActivity.kt
@@ -1,20 +1,16 @@
 package com.example.testusercentricstv
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
 
 /**
  * Loads [MainFragment].
  */
-class MainActivity : FragmentActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                .replace(R.id.main_browse_fragment, MainFragment())
-                .commitNow()
-        }
     }
 }
