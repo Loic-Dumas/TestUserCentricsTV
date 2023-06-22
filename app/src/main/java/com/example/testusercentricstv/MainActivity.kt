e82eaab535e0531e1d11ff887ed32a612cbe6bb2
Loïc Dumas <loic.dumas02@gmail.com>
2023-06-22 14:28:12 +0200

    Add User Centrics SDK.
    Initialize the sdk.
    When isReady, display first banner if shouldCollectConsent.
    Add two buttons to display first and second banner.

--- app/src/main/java/com/example/testusercentricstv/MainActivity.kt
+++ app/src/main/java/com/example/testusercentricstv/MainActivity.kt
@@ -2,15 +2,73 @@ package com.example.testusercentricstv
 
 import android.app.Activity
 import android.os.Bundle
-import androidx.fragment.app.FragmentActivity
+import android.util.Log
+import android.widget.Button
+import com.usercentrics.sdk.Usercentrics
+import com.usercentrics.sdk.UsercentricsBanner
+import com.usercentrics.sdk.UsercentricsOptions
 
 /**
- * Loads [MainFragment].
+ * Activity which init user centrics and display banners.
+ * The settings ID relates to a simple category, with various services regrouped in multiple categories (essential and non essential)
+ * Two buttons can display first and second banner.
+ *
+ * Issue : navigation in banner on TV, is really difficult.
+ * 1. Focused is not showed properly
+ * 2. Impossible to unselect a category
  */
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initUI()
+
+        initUserCentrics()
+    }
+
+    private fun initUI() {
+        val button1 = findViewById<Button>(R.id.button1)
+        val button2 = findViewById<Button>(R.id.button2)
+
+        button1.setOnClickListener { displayFirstLayer() }
+        button2.setOnClickListener { displaySecondLayer() }
+    }
+
+    private fun initUserCentrics() {
+
+        val options = UsercentricsOptions(settingsId = "jDc-I_sCxAwZkf")
+
+        Log.d("TestUserCentrics", "Start Init User Centrics")
+        Usercentrics.initialize(this, options)
+
+        Usercentrics.isReady({ status ->
+            if (status.shouldCollectConsent) {
+                displayFirstLayer()
+            } else {
+                applyConsent()
+            }
+        }, { error ->
+            Log.d("TestUserCentrics", "Error in isReady $error")
+        })
+    }
+
+    private fun displayFirstLayer() {
+        val banner = UsercentricsBanner(this)
+        banner.showFirstLayer { userResponse ->
+            applyConsent()
+        }
+    }
+
+    private fun displaySecondLayer() {
+        val banner = UsercentricsBanner(this)
+        banner.showSecondLayer { userResponse ->
+            applyConsent()
+        }
+    }
+
+    private fun applyConsent() {
+        Log.d("TestUserCentrics", "Apply consent")
     }
 }
